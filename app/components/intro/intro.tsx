0da42ce50b126b1c68d70f1fdff43591dc9e38b3
Xihao Zhu <xihao.zhu@outlook.com>
2025-12-08 17:08:26 +0000

    no more work 8/12

---
+++ app/components/intro/intro.tsx
@@ -0,0 +1,15 @@
+export default function Intro() {
+    return (
+        <div className="relative w-screen h-screen">
+            <div className="text-3xl font-bold mb-4">
+                Follow the ball
+            </div>
+            <div>
+                It tells you about
+            </div>
+            <div className="absolute bottom-[-12vw] left-[-5vw] text-[calc(100vw*1.05/4)] whitespace-nowrap tracking-[-0.15em] font-inter">
+                XIHAO ZHU
+            </div>
+        </div>
+    );
+}
